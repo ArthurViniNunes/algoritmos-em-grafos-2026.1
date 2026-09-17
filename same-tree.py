95a28db476b473be09f3104f762066991356df9d
Arthur Vinicius Carneiro Nunes <123214565+ArthurViniNunes@users.noreply.github.com>
2026-09-17 10:22:48 -0300

    Leetcoe problem that uses BFS idea

---
+++ same-tree.py
@@ -0,0 +1,29 @@
+# Solution of: https://leetcode.com/problems/same-tree/
+# By Arthur Nunes
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        q1 = deque([p])
+        q2 = deque([q])
+
+        while q1 and q2:
+            for _ in range(len(q1)):
+                nodeP = q1.popleft()
+                nodeQ = q2.popleft()
+
+                if nodeP is None and nodeQ is None:
+                    continue
+                if nodeP is None or nodeQ is None or nodeP.val != nodeQ.val:
+                    return False
+
+                q1.append(nodeP.left)
+                q1.append(nodeP.right)
+                q2.append(nodeQ.left)
+                q2.append(nodeQ.right)
+
+        return True
